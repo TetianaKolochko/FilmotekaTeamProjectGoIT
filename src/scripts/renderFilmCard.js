4b7544c6cc1bca7dc3c9003fe9c55c0fb2401349
EugeneNorko <zhenia020221@gmail.com>
2022-06-05 21:23:04 +0300

    addRenderFilmSet

--- src/scripts/renderFilmCard.js
+++ src/scripts/renderFilmCard.js
@@ -10,7 +10,30 @@ popularFilm().then((filmSet) => {
 
 export function renderFilmCardHtml(filmArray) {
     const renderHtml = filmArray.reduce((html, film) => {
-        return html += `<ul class=""><li>${film.title}</li></ul>`
+        return html +=
+        `<li class="gallery__item">
+          <div class="movie-card">
+            <img
+              class="poster"
+              src="https://image.tmdb.org/t/p/original/${film.poster_path}"
+              alt="poster of movie"
+              data-id="${film.id}"
+              loading="lazy"
+            />
+
+            <div class="movie-details">
+              <div class="movie-details-info">
+                <p class="movie-name">${film.title}</p>
+                <div class="movie-description">
+                  <p class="movie-info">Documentary, Drama, Other | 2019</p>
+                  <div class="movie-details-rate">6.5</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>`
     }, "");
     return place.insertAdjacentHTML('beforeend', renderHtml); 
 }
+
+//src="https://upload.wikimedia.org/wikipedia/commons/c/c2/No_image_poster.png"
