e43f00d8ca13273ca984ba36bc1dc9d6ac59bc52
EugeneNorko <zhenia020221@gmail.com>
2022-06-07 10:08:30 +0300

    changeTemplate

--- src/scripts/renderFilmCard.js
+++ src/scripts/renderFilmCard.js
@@ -1,27 +1,26 @@
 import { popularFilm } from '../scripts/fetch.js'
 import { refs } from './refs.js';
 
-const gallery = document.querySelector('.gallery');
 // import movieCardTemplate from '.././templates/cards.hbs';
 
 popularFilm().then((filmSet) => {
     console.log(filmSet)
     const filmArray = filmSet.results;
-    renderFilmCardHtml(filmArray);
+    renderMovieCardOnMainPage(filmArray);
 });
 
-export function renderFilmCardHtml(filmArray) {
+export function renderMovieCardOnMainPage(filmArray) {
 
   const markup = filmArray.reduce((html, film) => {
   const { original_title, poster_path, genre_ids, id, release_date } = film;
         return html +=
         `<li class="gallery__item">
-          <div class='movie-card'>
-            <img class='poster' src='https://image.tmdb.org/t/p/w500/${poster_path}' loading="lazy" alt='Poster for film ${original_title}' data-id=${id} />
-            <div class="movie-details">
-              <p class="movie-name">${original_title}</p>
-              <p class="movie-info">${genre_ids} | ${release_date}</p>
-            </div>
+          <div class='gallery__poster-wrapp'>
+            <img class='gallery__poster' src='https://image.tmdb.org/t/p/w500/${poster_path}' loading="lazy" alt='Poster for film ${original_title}' data-id=${id} />
+          </div>
+          <div class="gallery__movie-details">
+            <p class="movie-details__movie-name">${original_title}</p>
+            <p class="movie-details__movie-info">${genre_ids} | ${release_date}</p>
           </div>
         </li>`
     }, "");
