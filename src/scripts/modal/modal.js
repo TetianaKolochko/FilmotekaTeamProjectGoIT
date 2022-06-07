22497070ef97ad33627d0e6df54094e72eecd5ed
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-06-07 16:27:32 +0300

    push close modal ESC

--- src/scripts/modal/modal.js
+++ src/scripts/modal/modal.js
@@ -12,12 +12,12 @@ function openModal(e) {
   findCardId(e.target.dataset.id)
       .then(movie => {        
       if (e.target.nodeName !== 'IMG') return;
-      const markup = `<img class="gallery__image" src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="${movie.title}" loading="lazy" width="300" height="200"/>  <div class="info">    <p class="info-item">      <b class="gallery__info"> ${movie.title}</b>    </p>  </div>`;
+      const markup = `<img class="gallery__image" src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="${movie.title}" loading="lazy" width="500" />  <div class="modal-close-btn">Close</div>`;
       const modal = basicLightbox.create(markup);
 
       modal.show();
 
-      /* const closeBtn = document.querySelector('.modal-close-btn');
+      const closeBtn = document.querySelector('.modal-close-btn');
       closeBtn.addEventListener('click', closeModal);
 
       window.addEventListener('keydown', closeModalHandler);
@@ -31,10 +31,8 @@ function openModal(e) {
         function closeModal(e) {
         modal.close();
         window.removeEventListener('keydown', closeModalHandler);
-      } */
-
-      //new Function
-      //initStorageBtns();
+      }      
+      
     })
     .then(data => {})
     .catch(error => {
