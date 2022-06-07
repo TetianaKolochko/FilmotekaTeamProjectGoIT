e68f2e61244642d18465ca7f633488ad93cea878
EugeneNorko <zhenia020221@gmail.com>
2022-06-07 19:27:20 +0300

    addGenredMainPage

--- src/scripts/renderFilmCard.js
+++ src/scripts/renderFilmCard.js
@@ -1,7 +1,6 @@
 import { popularFilm } from '../scripts/fetch.js'
 import { refs } from './refs.js';
-
-// import movieCardTemplate from '.././templates/cards.hbs';
+import { GENRES } from './genre.js';
 
 popularFilm().then((filmSet) => {
     console.log(filmSet)
@@ -10,22 +9,36 @@ popularFilm().then((filmSet) => {
 });
 
 export function renderMovieCardOnMainPage(filmArray) {
-
   const markup = filmArray.reduce((html, film) => {
-  const { original_title, poster_path, genre_ids, id, release_date } = film;
+    const { original_title, poster_path, genre_ids, id, release_date } = film;
+    const genresArray = getGenresToId(genre_ids);
+    const genresText = sliceGenres(genresArray);
         return html +=
         `<li class="gallery__item">
           <a class="gallery__link">
             <img class='gallery__poster' src='https://image.tmdb.org/t/p/w500/${poster_path}' loading="lazy" alt='Poster for film ${original_title}' data-id=${id} />
-          <div class="gallery__movie-details">
-            <p class="movie-details__movie-name">${original_title}</p>
-            <p class="movie-details__movie-info">${genre_ids} | ${release_date}</p>
-          </div>
+            <div class="gallery__movie-details">
+              <p class="movie-details__movie-name">${original_title}</p>
+              <p class="movie-details__movie-info">${genresText} | ${release_date}</p>
+            </div>
           </a>
         </li>`
     }, "");
-
     return refs.movieGallery.insertAdjacentHTML('beforeend', markup);
 }
 
 //src="https://upload.wikimedia.org/wikipedia/commons/c/c2/No_image_poster.png"
+
+
+export function getGenresToId(idArray) {
+  return idArray.map(genreId => GENRES[genreId]);
+}
+
+function sliceGenres(genreArray) {
+  if (genreArray.length > 2) {
+    const slicedGenredWordArray = genreArray.slice(0, 2);
+    slicedGenredWordArray.push('Other');
+    return slicedGenredWordArray.join(", ");
+  }
+  return genreArray.join(", ")
+}
