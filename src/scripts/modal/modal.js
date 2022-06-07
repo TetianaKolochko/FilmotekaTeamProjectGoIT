e450c158dfa4789d67952c681bd472664cbae02d
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-06-07 13:54:45 +0300

    push opening a modal

--- src/scripts/modal/modal.js
+++ src/scripts/modal/modal.js
@@ -1,21 +1,23 @@
 import { findCardId } from '../fetch.js';
+import { refs } from '../refs.js';
 
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
+refs.movieGallery.addEventListener('click', openModal);
+
 function openModal(e) {
   e.preventDefault();
-
-  fetchOneMovieInfo(e.target.dataset.id)
-    .then(data => {
+    console.log(e.target.dataset.id)
+  findCardId(e.target.dataset.id)
+      .then(movie => {        
       if (e.target.nodeName !== 'IMG') return;
-
-      const markup = modalFilmCard(data);
+      const markup = `<img class="gallery__image" src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="${movie.title}" loading="lazy" width="300" height="200"/>  <div class="info">    <p class="info-item">      <b class="gallery__info"> ${movie.title}</b>    </p>  </div>`;
       const modal = basicLightbox.create(markup);
 
       modal.show();
 
-      const closeBtn = document.querySelector('.modal-close-btn');
+      /* const closeBtn = document.querySelector('.modal-close-btn');
       closeBtn.addEventListener('click', closeModal);
 
       window.addEventListener('keydown', closeModalHandler);
@@ -29,7 +31,7 @@ function openModal(e) {
         function closeModal(e) {
         modal.close();
         window.removeEventListener('keydown', closeModalHandler);
-      }
+      } */
 
       //new Function
       //initStorageBtns();
