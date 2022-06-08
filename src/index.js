bb0476b205df305704c27d74a193713912d837c5
TetianaKolochko <92599848+TetianaKolochko@users.noreply.github.com>
2022-06-08 16:07:30 +0300

    modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './scripts/modal/modal.js';
-import './scripts/modal/modalFilmCard.js';
 import './scripts/renderFilmCard.js';
 import './scripts/onOpenLibrary.js';
 import './scripts/searsh.js';
--- src/scripts/modal/modalFilmCard.js
+++ src/scripts/modal/modalFilmCard.js
@@ -1,57 +1,53 @@
+import { GENRES } from "../genre";
 
-const getMoviePage = () => {
-    const {
-        poster_path,
-        title,
-        vote_average,
-        vote_count,
-        popularity,
-        original_title,
-        overview,
-        genres,
-        id,
-    } = movie;
-
-    const genresList = genres => {
-      return genres.reduce((acc, item) => {
-        return (acc += `<li class="genre-list--item"><span>${item.name}</span></li>\n`);
-      }, "");
-    };
-     return `
-    <div class="container">
-    <section class="section" data-id="{id}">
-      <div class="poster-wrapper">
-        <img class="film-poster" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="poster image" />
-      </div>
-      <div class="description-wrapper">
-      <h2 class="film-title">${title}</h2>
-      
-      <div class="vote-wrapper">
-        <span class="titles vote-titles">vote / votes: </span>
-        <span class="votes-numbers">${vote_average} / ${vote_count}</span>
-      </div>
-      <div class="popularity-wrapper">
-        <span class="titles film-popularity">popularity: </span>
-        <span class="popularity-score">${popularity}</span>
-      </div>
-      <div class="original-film-wrapper">
-        <span class="titles original-film-titles">original title: </span>
-        <span class="original-film-name">${original_title}</span>
-      </div>
-      <div class="film-genre-wrapper">
-        <span class="titles film-genre">genre: </span>
-        <ul class="genre-list">${genresList(genres)}</ul>
-      </div>
-      <h3 class="about-movie">About</h3>
-      <p class="film-description">
-        ${overview}
-      </p>
-      <div class="button-wrapper">
-         <button type="button" class=" film__button btn__watch" data-id="${id}">Add to watched</button>
-         <button type="button" class=" film__button btn__queue" data-id="${id}">Add to queue</button>
-      </div>
-    </section>
-    </div>
-    `;
-  };
-
+export function createModalFilmCard({ movie }) {
+    console.log(movie);
+    return `
+    <div class="modal__backdrop"></div>
+     <div class="modal__container">
+     <button class="modal-close-btn">Close</button>
+     <div class="film__image">
+     
+        <img class="image" src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="${movie.title}" loading="lazy" width="500" />
+     </div>
+        <div class="film__information">
+           
+     <h2 class="film__title">${movie.title}</h2>
+     <ul>
+     <li class="film__item">
+                        <p class="film__details ">Vote / Votes</p>
+                        <p class="film__info--uper">
+                           <span class=" film__rating--orange">${movie.vote_average}</span>
+                           <span class="film__rating--divider"> / </span>
+                           <span>${movie.vote_count}</span>
+                       </p>
+                    </li>
+        <li class="film__item">
+                       <p class="film__details ">Popularity</p>
+                       <p class="film__info--uper">${movie.popularity}</p>
+                  </li>
+                   <li class="film__item">
+                         <p class="film__details">Original title</p>
+                         <p>${movie.original_title}</p>
+                    </li>
+                    <li class="film__item">
+                 <p class="film__details">Genre</p>
+                 <p class="film__info">
+              <span>${Object.values(GENRES).join(', ')}</span>
+                                                                              
+              </p>
+                 </li>
+                     </ul>
+                     </div>
+                     <div>
+                     <h3 class="film__about__title">About</h3>
+                     <p class="film__about__text">${movie.overview}</p>
+           </div>
+           <div class="film__button__wrapper">
+               <button type="button" class=" film__button btn__watch" data-id="">Add to watched</button>
+               <button type="button" class=" film__button btn__queue" data-id="">Add to queue</button>
+            </div>
+             </div>`;
+        
+}
+ 
