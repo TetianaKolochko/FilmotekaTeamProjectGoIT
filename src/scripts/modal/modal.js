32ed7565ad3bdda34abb9318c40bd50cd0d618b0
kriminskiy <93137873+kriminskiy@users.noreply.github.com>
2022-06-09 13:42:22 +0300

    addWathced

--- src/scripts/modal/modal.js
+++ src/scripts/modal/modal.js
@@ -1,6 +1,7 @@
 import { findCardId } from '../fetch.js';
 import { refs } from '../refs.js';
 import { createModalFilmCard } from './modalFilmCard.js';
+import addWatched from '../addWatched.js';
 
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
@@ -9,15 +10,15 @@ refs.movieGallery.addEventListener('click', openModal);
 
 function openModal(e) {
   e.preventDefault();
-    console.log(e.target.dataset.id)
+  console.log(e.target.dataset.id);
   findCardId(e.target.dataset.id)
-      .then(movie => {        
+    .then(movie => {
       if (e.target.nodeName !== 'IMG') return;
-    //   const markup = `<img class="gallery__image" src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="${movie.title}" loading="lazy" width="500" />  <div class="modal-close-btn">Close</div>`;
-      const modal = basicLightbox.create(createModalFilmCard({movie}));
+      //   const markup = `<img class="gallery__image" src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="${movie.title}" loading="lazy" width="500" />  <div class="modal-close-btn">Close</div>`;
+      const modal = basicLightbox.create(createModalFilmCard({ movie }));
 
       modal.show();
-
+      addWatched();
       const closeBtn = document.querySelector('.modal-close-btn');
       closeBtn.addEventListener('click', closeModal);
 
@@ -28,12 +29,11 @@ function openModal(e) {
           modal.close();
           window.removeEventListener('keydown', closeModalHandler);
         }
-        }
-        function closeModal(e) {
+      }
+      function closeModal(e) {
         modal.close();
         window.removeEventListener('keydown', closeModalHandler);
-      }      
-      
+      }
     })
     .then(data => {})
     .catch(error => {
--- src/scripts/modal/modalFilmCard.js
+++ src/scripts/modal/modalFilmCard.js
@@ -1,14 +1,16 @@
-import { GENRES } from "../genre";
+import { GENRES } from '../genre';
 
 export function createModalFilmCard({ movie }) {
-    console.log(movie);
-    return `
+  console.log(movie);
+  return `
     <div class="modal__backdrop"></div>
      <div class="modal__container">
      <button class="modal-close-btn">Close</button>
      <div class="film__image">
      
-        <img class="image" src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="${movie.title}" loading="lazy" width="500" />
+        <img class="image" src="https://image.tmdb.org/t/p/original/${
+          movie.backdrop_path
+        }" alt="${movie.title}" loading="lazy" width="500" />
      </div>
         <div class="film__information">
            
@@ -17,7 +19,9 @@ export function createModalFilmCard({ movie }) {
      <li class="film__item">
                         <p class="film__details ">Vote / Votes</p>
                         <p class="film__info--uper">
-                           <span class=" film__rating--orange">${movie.vote_average}</span>
+                           <span class=" film__rating--orange">${
+                             movie.vote_average
+                           }</span>
                            <span class="film__rating--divider"> / </span>
                            <span>${movie.vote_count}</span>
                        </p>
@@ -44,10 +48,10 @@ export function createModalFilmCard({ movie }) {
                      <p class="film__about__text">${movie.overview}</p>
            </div>
            <div class="film__button__wrapper">
-               <button type="button" class=" film__button btn__watch" data-id="">Add to watched</button>
+               <button type="button" class=" film__button btn__watch" data-id="${
+                 movie.id
+               }">Add to watched</button>
                <button type="button" class=" film__button btn__queue" data-id="">Add to queue</button>
             </div>
              </div>`;
-        
 }
- 
