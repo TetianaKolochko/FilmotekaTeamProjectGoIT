3701feb668760d33a78991ad826e512589855d93
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-06-10 14:53:57 +0300

    fix-modal

--- src/scripts/renderFilmCard.js
+++ src/scripts/renderFilmCard.js
@@ -19,11 +19,11 @@ export function renderMovieCardOnMainPage(filmArray) {
     const genresText = sliceGenres(genresArray);
      return html +=
         `<li class="gallery__item">
-          <a class="gallery__link" href="">
+          <a class="gallery__link" href="" data-id=${id}>
             <img class='gallery__poster' src='https://image.tmdb.org/t/p/w500/${poster_path}' loading="lazy" alt='Poster for film ${original_title}' data-id=${id} />
             <div class="gallery__movie-details">
-              <p class="movie-details__movie-name">${original_title}</p>
-              <p class="movie-details__movie-info">${genresText} | ${release_date}</p>
+              <p class="movie-details__movie-name" data-id=${id}>${original_title}</p>
+              <p class="movie-details__movie-info" data-id=${id}>${genresText} | ${release_date}</p>
             </div>
           </a>
         </li>`
