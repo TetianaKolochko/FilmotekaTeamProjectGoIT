cfa158625aa46eb0190ff1148b2f8e42006a3415
TetianaKolochko <92599848+TetianaKolochko@users.noreply.github.com>
2022-06-11 00:34:57 +0300

    g

--- src/scripts/modal/modalFilmCard.js
+++ src/scripts/modal/modalFilmCard.js
@@ -55,7 +55,7 @@ export function createModalFilmCard({ movie }) {
               <button class="modal-close-btn">close
      <svg class="close-icon" width="20" height="20">
  
-<path stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="2.1333" d="M8.533 8.533l14.933 14.933"></path>
+<path fill="#ff6b08" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="2.1333" d="M8.533 8.533l14.933 14.933"></path>
 <path stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="2.1333" d="M8.533 23.467l14.933-14.933"></path>
 
      </svg>
