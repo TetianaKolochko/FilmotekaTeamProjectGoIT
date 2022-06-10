5801bf310c53a1f55de005e015391f5d308fb1fe
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-06-10 18:05:45 +0300

    init footer_modal1

--- src/scripts/modal/footerModal.js
+++ src/scripts/modal/footerModal.js
@@ -1,77 +1,49 @@
-import teamId from '../team';
+import{ teamId }  from '../team';
 
 import * as basicLightbox from 'basiclightbox' 
 import 'basiclightbox/dist/basicLightbox.min.css';
 
-const autorsContainer = document.querySelector('a');
 const btnTeam = document.querySelector('.footer-text__command');
-function autorsCardsMarkup(teamId){
-    return teamId
-    .map(({ name, position, github, instagram }) => {
-     return `
-    <li class="worker__item">
-        <div class="worker__card">
-        <h2 class="worker__title">${name}</h2>
-        <div class="worker__icons">
-            ${
-                position
-                  ? ` <a class="worker__icon" href="${position}"></a>`
-                  : ''
-              }
-              ${
-                github
-                  ? ` <a class="worker__icon" href="${github}">
-                      <svg width="35" height="35">
-                        <use href="${links}#github"></use>
-                      </svg>
-                    </a>`
-                  : ''
-              }
-              ${
-                instagram
-                  ? ` <a class="workers__icon" href="${instagram}">
-                      <svg width="35" height="35">
-                        <use href="${links}#facebook"></use>
-                      </svg>
-                    </a>`
-                  : ''
-              }
+const autorsCardsMarkup = teamId
+.map(({ name, position, github, instagram }) => {
+        return `
+        <li class="worker__item">
+              <div class="worker__card">
+                <h2 class="worker__title">${name}</h2>
+                <div class="worker__icons">
+                <p class = "worker__position">${position}</p>  
+                <a class="worker__icon" href="${github}">
+                          <svg width="35" height="35">
+                            <use href="#github"></use>
+                          </svg>
+                        </a>
+                  <a class="workers__icon" href="${instagram}">
+                          <svg width="35" height="35">
+                            <use href="#instagram"></use>
+                          </svg>
+                        </a>
+                </div>
             </div>
-        </div>
-      </li>`;
-
+          </li>`
 })
-.join(` `);
-}
+.join(``);
 
-autorsContainer.insertAdjacentHTML('beforeend', autorsCardsMarkup);
 btnTeam.addEventListener('click', modalShow);
 
 function modalShow(e){
     e.preventDefault();
     
-    const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
-        <input placeholder="Type something">
-        <a>Close</a>
-    </div>
-`, {
-    onShow: (instance) => {
-        instance.element().querySelector('a').onclick = instance.close
-    }
-})
+    const instance = basicLightbox.create(autorsCardsMarkup);
+    instance.show();
 
-instance.show()
+    closeModal(e);
+}
 
-    if(modal.visible()) {
-        window.addEventListener(`keydown`, (event) => {
-            console.log(event);
-            if(event.code === `Escape`) {
-                modal.close();
-            };
-        });
-    };
+function closeModal(e) {
+    if (e.code === 'Escape') {
+      modal.close();
+      window.removeEventListener('keydown', closeModal);
+    } 
 }
 
 
@@ -79,20 +51,14 @@ instance.show()
 
 
 
-
-
-
-
-// import team from "../team";
-
-
-// export function ourCardTeam ({ name, position, github, instagram }){
-//     return `
+// function autorsCardsMarkup(teamId){
+//     return teamId
+//     .map(({ name, position, github, instagram }) => {
+//      return `
 //     <li class="worker__item">
-        
-//           <div class="worker__card">
-//             <h2 class="worker__title">${name}</h2>
-//             <div class="worker__icons">
+//         <div class="worker__card">
+//         <h2 class="worker__title">${name}</h2>
+//         <div class="worker__icons">
 //             ${
 //                 position
 //                   ? ` <a class="worker__icon" href="${position}"></a>`
@@ -111,16 +77,28 @@ instance.show()
 //                 instagram
 //                   ? ` <a class="workers__icon" href="${instagram}">
 //                       <svg width="35" height="35">
-//                         <use href="${links}#facebook"></use>
+//                         <use href="${links}#instagram"></use>
 //                       </svg>
 //                     </a>`
 //                   : ''
 //               }
 //             </div>
 //         </div>
-//       </li>`
+//       </li>`;
 
+// })
+// .join(` `);
 // }
 
+  // if(modal.visible()) {
+    //     window.addEventListener(`keydown`, (event) => {
+    //         console.log(event);
+    //         if(event.code === `Escape`) {
+    //             modal.close();
+    //         };
+    //     });
+    // };
+
+    // const autorsContainer = document.querySelector('#js_team_modal');
 
 
