2fca8c7eecccc1ec20b03003cf59a87789fdc883
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-06-12 10:34:17 +0300

    add trailer

--- src/scripts/modal/modalTrailer.js
+++ src/scripts/modal/modalTrailer.js
@@ -1,23 +1,22 @@
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
-const movieTrailer = () => {
-  const btn = document.querySelector('.modal__container .trailer__btn');
+import { findTrailer } from '../fetch.js';
 
-  btn.addEventListener('click', function (e) {
+const movieTrailer = () => {
+  const trailerBtn = document.querySelector('.trailer__btn');
+
+  trailerBtn.addEventListener('click', function (e) {
         openTrailer(e.target.dataset.id);
   })
 }
 
 function openTrailer (id) {
-    const ApiKey = '1e3bd345eb5d29ac0f4521d096bb0e9d';
-    const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${ApiKey}&language=en-US`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const id = data.results[0].key;
+    findTrailer(id).then(data => {
+        console.log(data);
+        const key = data.results[0].key;
         const instance = basicLightbox.create(`
-  <iframe width="860" height="615" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe width="860" height="615" src="https://www.youtube.com/embed/${key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 `);
         instance.show();
         
