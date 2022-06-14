58b583ed272516e771a231205d0d78c1915904c1
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-06-14 13:59:05 +0300

    fix trailer

--- src/scripts/modal/modalTrailer.js
+++ src/scripts/modal/modalTrailer.js
@@ -12,16 +12,31 @@ const movieTrailer = () => {
 }
 
 function openTrailer (id) {
-    findTrailer(id).then(data => {
+  findTrailer(id).then(data => {
+    console.log(data);
         const key = data.results[0].key;
-        const instance = basicLightbox.create(`
+        const modal = basicLightbox.create(`
   <iframe width="860" height="615" src="https://www.youtube.com/embed/${key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-`);
-        instance.show();
-        
+<button class="modal-close-btn">
+     <svg class="close-modal__icon" width="20" height="20">
+ <path  stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="2.1333" d="M8.533 8.533l14.933 14.933"></path>
+<path stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="2.1333" d="M8.533 23.467l14.933-14.933"></path>
+     </svg>
+     </button>`);
+    modal.show();
+    
+    const closeBtn = document.querySelector('.modal-close-btn');
+    closeBtn.addEventListener('click', closeModal);
+    function closeModal(e) {
+        modal.close();        
+        window.removeEventListener('keydown', closeModalHandler);
+      }
       })
       .catch(error => {
-      console.log('oops!');
+      const modal = basicLightbox.create(`
+    <iframe width="860" height="615" src="https://www.youtube.com/embed/6DhiiFGk4_s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      `);
+        modal.show();
     });
   }
 
